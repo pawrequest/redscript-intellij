2ea4ffb6e360ed5411239b95ae37bca5d3878b0f
paw <pawrequest@users.noreply.github.com>
2025-06-25 19:20:50 +0100

    rm github custom in build

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,8 @@ repositories {
 //// Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
     testImplementation(libs.junit)
+    implementation(libs.org.json)
 
-    implementation("com.github.pawrequest:github:v0.0.1")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
